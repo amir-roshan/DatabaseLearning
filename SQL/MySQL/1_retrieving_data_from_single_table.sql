7c9c42984bd978df6b197e477fb09c315bb41d25
amir-roshan <amirvalielahi@gmail.com>
2024-07-12 15:51:22 -0700

    Learned and implemented IS NULL operator, ORDER BY clause, and LIMIT clause

--- SQL/MySQL/1_retrieving_data_from_single_table.sql
+++ SQL/MySQL/1_retrieving_data_from_single_table.sql
@@ -58,8 +58,74 @@ SELECT * FROM customers WHERE last_name LIKE '%b%';
 SELECT * FROM customers WHERE last_name LIKE '%y';
 
 -- the lastname should be 5 chars and the last one should be y
-SELECT * FROM customers WHERE last_name LIKE '____y'
+SELECT * FROM customers WHERE last_name LIKE '____y';
 
 -- % any number of characters 
 -- _ single number of character
 -- LIKE is an older operator
+
+-- ----------------------------------------
+
+-- The REGEXP Operator
+-- The last name ends with field.
+SELECT * FROM customers WHERE last_name REGEXP 'field';
+
+-- The last name must start with a field which is represented by carrot sign (^)
+SELECT * FROM customers WHERE last_name REGEXP '^field';
+
+-- The last name must start with a field or ends with mac or rose
+SELECT * FROM customers WHERE last_name REGEXP '^field|mac|rose';
+
+-- There is 'e' in the customer's last name => ge, ie, me
+SELECT * FROM customers WHERE last_name REGEXP '[gim]e';
+
+-- eq
+SELECT * FROM customers WHERE last_name REGEXP 'e[q]';
+
+-- Any chars from A - H before the letter e.
+SELECT * FROM customers WHERE last_name REGEXP '[a-h]e';
+
+-- ^ => Beginning 
+-- $ => End ("roshan$")
+-- | => Logical or
+-- [abcd] => targetting specific chars
+-- [a-f] => representing chars with a range 
+
+-- ----------------------------------------
+ 
+-- The IS NULL Operator 
+-- Customers who don't have any phone number	
+SELECT * FROM customers WHERE phone IS NULL;
+
+-- Customers who have a phone number	
+SELECT * FROM customers WHERE phone IS NOT NULL;
+
+-- ----------------------------------------
+
+-- The ORDER BY Clause
+-- In relation databases every table should have a primary key column
+-- By defualt the every table is sorted by the primary key column
+SELECT * FROM customers ORDER BY first_name;
+
+-- Order by descending
+SELECT * FROM customers ORDER BY first_name DESC;
+
+-- Order by state then first name
+SELECT * FROM customers ORDER BY state, first_name DESC;
+
+-- These queries are valid in MySQL
+SELECT first_name, last_name FROM customers ORDER BY birth_date;
+SELECT first_name, last_name, 10 AS points FROM customers ORDER BY points;
+
+SELECT first_name, last_name FROM customers ORDER BY 2, 1;
+
+-- ----------------------------------------
+
+-- The LIMIT Clause
+-- Return the first 3 customers
+-- if the limit number is larger than the actual number of cutsomer it will return all of the customers
+-- The LIMIT clause should always come at the end
+SELECT * FROM customers LIMIT 3; 
+
+-- The number 6 in here is the offset which tells MySQL to ignores the first 6 records
+SELECT * FROM customers LIMIT 6, 3; 
--- SQL/MySQL/1_retrieving_data_from_single_table_exercises.sql
+++ SQL/MySQL/1_retrieving_data_from_single_table_exercises.sql
@@ -8,14 +8,17 @@ SELECT
 	unit_price * 1.1 AS 'new unit price'
 FROM products;
 
+
 -- Get the orders placed this year
 SELECT * FROM orders WHERE YEAR(order_date) = 2019;
 -- OR
 SELECT * FROM orders WHERE order_date >= '2019-01-01';
 
+
 -- From the order_items table, get the items for order #6 
 -- Where the total price is greater than 30.
 SELECT * FROM order_items WHERE order_id = 6 AND unit_price * quantity > 30; 
+
 
 -- Return products with
 -- 		quantity in stock equal to 49, 38, 72
@@ -25,8 +28,38 @@ SELECT * FROM products WHERE quantity_in_stock IN (49, 38, 72);
 -- 		between 1/1/1990 and 1/1/2000
 SELECT * FROM customers WHERE birth_date BETWEEN '1990-01-01' AND '2000-01-01';
 
+
 -- Get the customers whose 
 -- 		addressess contain TRAIL or AVENUE 
 -- 		phone numbers end with 9 
 SELECT * FROM customers WHERE (address LIKE '%trail%' OR  address LIKE '%avenue%') AND phone LIKE "%9";
 SELECT * FROM customers;
+
+
+-- Get the Customers whose 
+-- 		First names are ELKA or AMBUR
+SELECT * FROM customers WHERE first_name REGEXP ("elka|ambur");
+-- 		Last names end with EY or ON
+SELECT * FROM customers WHERE last_name REGEXP ("ey$|on$");
+-- 		Last names start with MY or contains SE
+SELECT * FROM customers WHERE last_name REGEXP ("^my|^se");
+-- 		Last names contain B followed by R or U
+SELECT * FROM customers WHERE last_name REGEXP ("b[ru]");
+
+
+-- Get the orders that are not shipped
+SELECT * FROM orders WHERE shipped_date IS NULL;
+
+
+-- Order the items by their total price from the order_items and the order id of 2
+-- AS => alias which means AKA
+SELECT * , quantity * unit_price AS total_price
+FROM order_items
+WHERE order_id = 2 
+ORDER BY total_price DESC;
+
+
+-- Get the top 3 loayal customers
+SELECT * FROM customers
+ORDER BY points DESC
+LIMIT 3;
